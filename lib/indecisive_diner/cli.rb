b0fcb2745835debdf5d6d9c6ddbe2de51101f0de
OliviaJoyHanson <olivia.joy.hanson@gmail.com>
2021-01-02 14:57:55 -0500

    starting to scrape real data for scaper class

--- lib/indecisive_diner/cli.rb
+++ lib/indecisive_diner/cli.rb
@@ -6,6 +6,7 @@ class IndecisiveDiner::CLI
         @name = gets.strip.downcase
         puts "What's your location?"
         @location = gets.strip.downcase
+        # @location.split(/\s+/) if @location.split(/\s+/).length > 1
         #@user = User.new(@name, @location)
         sampled_restaurant
         menu
@@ -39,49 +40,14 @@ class IndecisiveDiner::CLI
                 puts "Spinning wheel..."
                 sampled_restaurant
                 puts "Wanna eat here? [yes, no, exit]"
-            else
+            elsif input != "no" || input != "yes" || input != "exit"
                 puts "Didn't quite get that... type yes, no, or exit"
             end 
         end
     end 
 
     def goodbye 
-        "See you next time, #{@name}!"
+        puts "See you next time, #{@name}!"
     end 
 
-    # def call
-    #   list_deals
-    #   menu
-    #   goodbye
-    # end
-  
-    # def list_deals
-    #   # here doc - http://blog.jayfields.com/2006/12/ruby-multiline-strings-here-doc-or.html
-    #   puts "Today's Daily Deals:"
-    #   @deals = DailyDeal::Deal.today
-    #   @deals.each.with_index(1) do |deal, i|
-    #     puts "#{i}. #{deal.name} - #{deal.price} - #{deal.availability}"
-    #   end
-    # end
-  
-    # def menu
-    #   input = nil
-    #   while input != "exit"
-    #     puts "Enter the number of the deal you'd like more info on or type list to see the deals again or type exit:"
-    #     input = gets.strip.downcase
-  
-    #     if input.to_i > 0
-    #       the_deal = @deals[input.to_i-1]
-    #       puts "#{the_deal.name} - #{the_deal.price} - #{the_deal.availability}"
-    #     elsif input == "list"
-    #       list_deals
-    #     else
-    #       puts "Not sure what you want, type list or exit."
-    #     end
-    #   end
-    # end
-  
-    # def goodbye
-    #   puts "See you tomorrow for more deals!!!"
-    # end
   end
--- lib/indecisive_diner/scraper.rb
+++ lib/indecisive_diner/scraper.rb
@@ -1,8 +1,17 @@
 require 'pry'
+require 'nokogiri'
+require 'open-uri'
 
 class IndecisiveDiner::Scraper 
 
     def self.scrape(location)
+        # if location.length > 1
+        doc = Nokogiri::HTML(URI.open("https://www.opentable.com/nearby/restaurants-near-me-fort-wayne"))
+
+        rest_box = doc.css("#mainContent div._3kGTRpUBze6qgm9OggF-31 div._2AVylkZna9eBnqoELI42uw div._2ZhFCF4LhPo6lVjuKJPqt- div[data-test]")
+
+        binding.pry
+        
         scraped = [["bakerstreet", "contemporary american", "4.8", "link1"], ["Nawa", "asian fusion", "5", "link2"]]
         @scraped_restaurants = []
         scraped.each do |restaurant|
