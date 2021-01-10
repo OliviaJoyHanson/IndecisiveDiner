3aa0fe9ffb77ace427c4af5e5c25fb9f07353698
OliviaJoyHanson <olivia.joy.hanson@gmail.com>
2021-01-10 16:19:08 -0500

    added page_count_check class method in scraper class to verify the existence of another page of restaurants to scrape from that location

--- lib/indecisive_diner/scraper.rb
+++ lib/indecisive_diner/scraper.rb
@@ -19,7 +19,22 @@ class IndecisiveDiner::Scraper
  
     end 
 
+    def self.page_count_check(location, count)
+        if location.count == 1
+            url = "https://www.opentable.com/nearby/restaurants-near-me-#{location.join}?prices=&cuisines=&corrid=77d80551-2985-4954-bb68-416c677f9ced&page=#{count}"
+        else
+            url = "https://www.opentable.com/nearby/restaurants-near-me-#{location.join("-")}?prices=&cuisines=&corrid=77d80551-2985-4954-bb68-416c677f9ced&page=#{count}"
+        end
+        
+            URI.open(url).status
+        rescue OpenURI::HTTPError
+            false
+    end 
+
     def self.scrape(location)
+        count = 1
+        # until !self.page_count_check(location, count)
+
         if location.count == 1
             @doc = Nokogiri::HTML(URI.open("https://www.opentable.com/nearby/restaurants-near-me-#{location.join}"))
         else
@@ -36,19 +51,13 @@ class IndecisiveDiner::Scraper
             card.children.each do |rest|
                 
                 name = rest.css("a h6").text.split(" ").drop(1).join(" ").strip
+
                 cuisine_array = rest.css("._2p0jcmKJSDjEh-wNrLIpzJ").text.downcase.split(" ") - location
-                
-                # cuisine = cuisine_array.reject {|e| [0, cuisine_array.length - 1].include? cuisine_array.index(e)}.map{|e| e.capitalize}.join(" ")
                 bubbles_indices = cuisine_array.each_index.select{|e| cuisine_array[e] == '•'} 
-                # cuisine_array.length
-                # if cuisine_array.index is larger than bubbles_indices[1], reject
-                # binding.pry
                 cuisine = cuisine_array.reject {|e| [bubbles_indices[0], bubbles_indices[1]].include?(cuisine_array.index(e)) || cuisine_array.index(e) > bubbles_indices[1]}.map{|e| e.capitalize}.join(" ")
-                # if element if array matches value of index 0, delete that index and everything after that until end of array length-1
                 
-                
-                # binding.pry
                 rating = rest.css("._2s6ofZ_eiTKuvNHV3mVnaG").text
+
                 link = rest.css("._1e9PcCDb012hY4BcGfraQB").attr("href").value
                 
                 @scraped_restaurants_array << IndecisiveDiner::Restaurant.new(name, cuisine, rating, link)
