5b4e99751576fd697e3f76e14d89720525efac46
OliviaJoyHanson <olivia.joy.hanson@gmail.com>
2021-01-10 15:31:52 -0500

    fixed cuisine scrape to only include cuisine type and no non-word characters or superfluous words that are included in the scraped string

--- lib/indecisive_diner/scraper.rb
+++ lib/indecisive_diner/scraper.rb
@@ -7,65 +7,25 @@ require "net/http"
 class IndecisiveDiner::Scraper 
 
     def self.location_check(location)
-        url = "https://www.opentable.com/nearby/restaurants-near-me-#{location.join}"
-
+        if location.count == 1
+            url = "https://www.opentable.com/nearby/restaurants-near-me-#{location.join}"
+        else
+            url = "https://www.opentable.com/nearby/restaurants-near-me-#{location.join("-")}"
+        end
         
             URI.open(url).status
         rescue OpenURI::HTTPError
             false
  
-
-        # binding.pry
-        # Net::HTTP.get_response(url).is_a?(Net::HTTPSuccess)
-
-        # response = HTTParty.get('http://google.com', follow_redirects: true)
-
-        # response = nil
-        # seen = Set.new
-        # loop do
-        #     url = URI.parse(url)
-        #     break if seen.include? url.to_s
-        #     break if seen.size > max_redirects
-        #     seen.add(url.to_s)
-        #     response = Net::HTTP.new(url.host, url.port).request_head(url.path)
-        #     if response.kind_of?(Net::HTTPRedirection)
-        #     url = response['location']
-        #     else
-        #     break
-        #     end
-        # end
-        # response.kind_of?(Net::HTTPSuccess) && url.to_s
-
-        # url = URI.parse(url_string)
-        # req = Net::HTTP.new(url.host, url.port)
-        # req.use_ssl = (url.scheme == 'https')
-        # path = url.path if url.path.present?
-        # res = req.request_head(path || '/')
-        # res.code != "404" # false if returns 404 - not found
-        # rescue Errno::ENOENT
-        # false # false if can't find the server
-
-        # uri = URI.parse(url)
-        # uri.is_a?(URI::HTTP) && !uri.host.nil?
-        # binding.pry
-        # rescue URI::InvalidURIError
-        # false
-
-        # uri = URI.parse(url)
-        # Net::HTTP.start(uri.host, uri.port) do |http|
-        #     binding.pry
-        #     return http.head(uri.request_uri).code == "200"
-        # end
-        
-        # req = Net::HTTP.new(url.host, url.port)
-        # req.use_ssl = true if url.scheme == 'https'
-        # res = req.request_head(url.path)
- 
     end 
 
     def self.scrape(location)
-        # if location.length > 1
-        @doc = Nokogiri::HTML(URI.open("https://www.opentable.com/nearby/restaurants-near-me-fort-wayne"))
+        if location.count == 1
+            @doc = Nokogiri::HTML(URI.open("https://www.opentable.com/nearby/restaurants-near-me-#{location.join}"))
+        else
+            @doc = Nokogiri::HTML(URI.open("https://www.opentable.com/nearby/restaurants-near-me-#{location.join("-")}"))
+        end
+        # @doc = Nokogiri::HTML(URI.open("https://www.opentable.com/nearby/restaurants-near-me-fort-wayne"))
         # binding.pry
         rest_cards = @doc.css('div[data-test="restaurant-cards"]')
         # binding.pry
@@ -77,7 +37,16 @@ class IndecisiveDiner::Scraper
                 
                 name = rest.css("a h6").text.split(" ").drop(1).join(" ").strip
                 cuisine_array = rest.css("._2p0jcmKJSDjEh-wNrLIpzJ").text.downcase.split(" ") - location
-                cuisine = cuisine_array.reject {|e| [0, cuisine_array.length - 1].include? cuisine_array.index(e)}.map{|e| e.capitalize}.join(" ")
+                
+                # cuisine = cuisine_array.reject {|e| [0, cuisine_array.length - 1].include? cuisine_array.index(e)}.map{|e| e.capitalize}.join(" ")
+                bubbles_indices = cuisine_array.each_index.select{|e| cuisine_array[e] == '•'} 
+                # cuisine_array.length
+                # if cuisine_array.index is larger than bubbles_indices[1], reject
+                # binding.pry
+                cuisine = cuisine_array.reject {|e| [bubbles_indices[0], bubbles_indices[1]].include?(cuisine_array.index(e)) || cuisine_array.index(e) > bubbles_indices[1]}.map{|e| e.capitalize}.join(" ")
+                # if element if array matches value of index 0, delete that index and everything after that until end of array length-1
+                
+                
                 # binding.pry
                 rating = rest.css("._2s6ofZ_eiTKuvNHV3mVnaG").text
                 link = rest.css("._1e9PcCDb012hY4BcGfraQB").attr("href").value
